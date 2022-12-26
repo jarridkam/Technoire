b66f8a92a21bb86c9d1eac85484f9a9218978bc7
jarridkam <73915267+jarridkam@users.noreply.github.com>
2022-12-26 13:07:09 -0800

    Add files via upload

---
+++ Scripts/Character/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Technoire
+{
+    public class Player : CharacterBase
+    {
+        public int charismaInit;
+
+        public enum PlayerClass
+        {
+            StreetSamurai, SlumShaman, Techie, Fixer, RockerBoy, Nomad, Corp
+        };
+
+        public PlayerClass playerClass;
+
+        protected int charisma;
+
+        public override void SetCharacter(CharacterData characterData)
+        {
+            this.charName = characterData.charName;
+
+            this.health = characterData.health;
+            this.vril = characterData.vril;
+
+            this.strength = characterData.strength;
+            this.endurace = characterData.endurace;
+            this.intelligence = characterData.intelligence;
+            this.wisdom = characterData.wisdom;
+            this.agility = characterData.agility;
+            this.luck = characterData.luck;
+            this.charisma = this.charismaInit;
+        }
+    }
+}
+
